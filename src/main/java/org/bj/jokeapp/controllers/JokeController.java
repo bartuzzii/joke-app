43a9e362dc797b42ba5b2e4d004ae65b57e378ad
Bart <bartlomiej.jedruszak@pollub.edu.pl>
2018-06-16 15:15:17 +0200

    Final

--- src/main/java/org/bj/jokeapp/controllers/JokeController.java
+++ src/main/java/org/bj/jokeapp/controllers/JokeController.java
@@ -1,4 +1,26 @@
 package org.bj.jokeapp.controllers;
 
+import org.bj.jokeapp.services.JokeService;
+import org.bj.jokeapp.services.JokeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class JokeController {
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+    @GetMapping
+    public String getJoke(Model model){
+        model.addAttribute("joke",jokeService.joke());
+        return "chucknorris";
+    }
+
 }
--- src/main/java/org/bj/jokeapp/services/JokeService.java
+++ src/main/java/org/bj/jokeapp/services/JokeService.java
@@ -1,4 +1,8 @@
 package org.bj.jokeapp.services;
 
+
+
+
 public interface JokeService {
+   String joke();
 }
